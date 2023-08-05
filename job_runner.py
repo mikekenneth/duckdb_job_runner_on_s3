82dcb2d1fdbf6276a59dbd7ea1ecfe5acc387692
mikekenneth <mike.kenneth47@gmail.com>
2023-08-05 22:41:14 +0100

    first commit

---
+++ job_runner.py
@@ -0,0 +1,23 @@
+import duckdb
+import psycopg2
+import logging
+
+def get_new_job() -> dict:
+    ...
+    
+
+def update_job(job: dict) -> bool:
+    ...
+
+
+def main():
+    # Get New Job
+    
+    # Run Job on DuckDB
+    
+    # Update Job Status
+    pass
+
+
+if __name__ == "__main__":
+    
